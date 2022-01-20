c06e74ec830a8e7005fc330d4cfc018758d9be77
simranjeetsingh27 <97332641+simranjeetsingh27@users.noreply.github.com>
2022-01-21 01:59:10 +0530

    Update mavengoals.groovy

--- vars/mavengoals.groovy
+++ vars/mavengoals.groovy
@@ -1,7 +1,9 @@
+def clone(Map config = [:]) (
+git branch: "$(config.branch)", url: "$(config.giturl)"
+)
 def clean(){
   sh 'mvn clean'
 }
-
 def compile(){
   sh 'mvn compile'
 }
@@ -10,4 +12,8 @@ def stabiltity(){
 }
 def quality(){
   sh 'mvn checkstyle:checkstyle'
+}
+def report(){
+recordIssues(tools: [checkStyle()])
+publishCoverage adapters: [coberturaAdapter('')], sourceFileResolver: sourceFiles('NEVER_STORE')
 }
